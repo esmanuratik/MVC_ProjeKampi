bc57cfb937590caa9b67e58e18ac6c1f0aeeb2fd
esmanuratik <atikesmanur@gmail.com>
2023-05-10 00:10:39 +0300

    yazar işlmeleri eklendi ve bitti

--- MVC_ProjeKampi/Controllers/WriterController.cs
+++ MVC_ProjeKampi/Controllers/WriterController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +14,63 @@ namespace MVC_ProjeKampi.Controllers
     public class WriterController : Controller
     {
         WriterManager wm = new WriterManager(new EF_WriterDAL());
+        Writer_Validatior writervalidator = new Writer_Validatior();
         public ActionResult Index()
         {
             var WriterValues = wm.GetList();
             return View(WriterValues);
+        }
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+
+            ValidationResult results = writervalidator.Validate(p);
+
+            if (results.IsValid)
+            {
+                wm.WriterAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult EditWriter(int id)
+        {
+            var writervalue=wm.GetByID(id);
+            return View(writervalue);
+        }
+        [HttpPost]
+        public ActionResult EditWriter(Writer p)
+        {
+            ValidationResult results = writervalidator.Validate(p);
+
+            if (results.IsValid)//Eğer Validate geçerliyse update et.ValidationMessageların view de çalışması için bunu eklemeliyim.
+            {
+                wm.WriterUpdate(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
         }
     }
 }
