7ee011aa21f196f05821143a08f60b9ad38b38c4
esmanuratik <atikesmanur@gmail.com>
2023-05-27 23:28:50 +0300

    Mesaj İşlemleri Tamamlandı

---
+++ BusinessLayer/Abstract/IMessage_Service.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IMessage_Service
+    {
+        List<Message> GetListInbox();   //gelen message sınıfına ait dizinimi dahil etmiş oldum.
+        List<Message> GetListSendbox();   //gönderilen message sınıfına ait dizinimi dahil etmiş oldum.
+        void MessageAdd(Message message);
+        //id yi silmek için bulma işleminin metotu (imzası atılacak) yazılacak
+        Message GetByID(int id);
+        void MessageDelete(Message message);//silme işlemini tanımlamış oldum. 
+        void MessageUpdate(Message message);
+    }
+}
---
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,58 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class MessageManager : IMessage_Service
+    {
+        IMessage_DAL _messageDal; //bir field oluşturdum bu field a değer ataması yapmam için ctor oluşturmalıyım.
+
+        public MessageManager(IMessage_DAL messageDal)
+        {
+            _messageDal = messageDal;
+        }
+
+        public Message GetByID(int id)
+        {
+            return _messageDal.Get(x => x.MessageID == id);
+        }
+
+        //public List<Message> GetList()//burada getlist dediğimizde bütün mesajlar geliyor bunun için getlistInbox ve GetlistSendBox olarak ikiye ayırıyorum.
+        //{
+        //    return _messageDal.List(x=>x.ReceiverMail=="admin@gmail.com");
+        //    //mesaj iletilme kısmında mesajı alabilmek için receivermaile ihtiyacım var yani alıcı mail.Fakat bu sağlıklı bir yöntem değil.Sadece ilgili değerin çalışıp çalışmadığı kontrol edilicecek
+        //}
+
+        public List<Message> GetListInbox()
+        {
+            return _messageDal.List(x => x.ReceiverMail == "admin@gmail.com");//alıcı 
+            //mesaj iletilme kısmında mesajı alabilmek için receivermaile ihtiyacım var yani alıcı mail.Fakat bu sağlıklı bir yöntem değil.Sadece ilgili değerin çalışıp çalışmadığı kontrol edilicecek
+        }
+
+        public List<Message> GetListSendbox()//gönderen
+        {
+            return _messageDal.List(x => x.SenderMail == "admin@gmail.com");
+        }
+
+        public void MessageAdd(Message message)//yeni bir mesaj ekleyebilmek için 
+        {
+            _messageDal.Insert(message);
+        }
+
+        public void MessageDelete(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageUpdate(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
