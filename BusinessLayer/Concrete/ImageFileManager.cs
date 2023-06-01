cbd0bdb3f345a052367076fb6f50ac2ca33f407e
esmanuratik <atikesmanur@gmail.com>
2023-06-01 23:30:09 +0300

    yeni mesaj ekleme ve admin login teması yapıldı

---
+++ BusinessLayer/Concrete/ImageFileManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ImageFileManager : IImageFile_Service
+    {
+        IImageFile_DAL _ımageFile_DAL;
+
+        public ImageFileManager(IImageFile_DAL ımageFile_DAL)
+        {
+            _ımageFile_DAL = ımageFile_DAL;
+        }
+
+        public List<ImageFile> GetList()
+        {
+            return _ımageFile_DAL.List();
+        }
+    }
+}
---
+++ MVC_ProjeKampi/Controllers/GalleryController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_ProjeKampi.Controllers
+{
+    public class GalleryController : Controller
+    {
+        ImageFileManager ifm = new ImageFileManager(new EF_ImageFileDAL()); 
+        public ActionResult Index()
+        {
+            var files = ifm.GetList();
+            return View(files);
+        }
+    }
+}
